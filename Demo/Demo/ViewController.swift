ae5cda166d7e786160d4291637be7c760f93a478
加賀谷諒 <ryokagaya0624@gmail.com>
2018-02-11 14:47:11 +0900

    create Project

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by 加賀谷諒 on 2018/02/11.
+//  Copyright © 2018年 ryo kagaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SCDropDownMenu/SCDropDownMenu.h
@@ -0,0 +1,19 @@
+//
+//  SCDropDownMenu.h
+//  SCDropDownMenu
+//
+//  Created by 加賀谷諒 on 2018/02/11.
+//  Copyright © 2018年 ryo kagaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SCDropDownMenu.
+FOUNDATION_EXPORT double SCDropDownMenuVersionNumber;
+
+//! Project version string for SCDropDownMenu.
+FOUNDATION_EXPORT const unsigned char SCDropDownMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCDropDownMenu/PublicHeader.h>
+
+
